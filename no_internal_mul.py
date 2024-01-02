eb332ba837568ab33441547c1c30d6131e9a7fb6
KaylaBytes <144823521+KaylaBytes@users.noreply.github.com>
2024-01-02 06:59:24 +0000

    Removed remaining built-in multiplication/division

--- no_internal_mul.py
+++ no_internal_mul.py
@@ -11,20 +11,28 @@ except ValueError:
 start_time = time.perf_counter()
 
 # Shift numbers to make them integers
+
 def value_shift(value):
     global shift
-    while value % 1 != 0:
-        shift_value = value
-        print("shift value is", shift_value)
-        for x in range(9): # multiplies by 10
-            value += shift_value
-            print(f"thingy {x}: {value}")
-        shift += 1
-    return int(value)
+    split_value = list(str(value))
+    largest_decimal = 0
+    for x in range(len(split_value)):
+        digit = split_value[-(x + 1)]
+        try:
+            if int(digit) > largest_decimal: 
+                largest_decimal = int(digit)
+        except ValueError:
+            if largest_decimal != 0: 
+                shift += x
+                return str(value).replace(".", "")
+            else:
+                return str(value).replace(".0", "")
+
+# potentially change to reversing the text so decimal point is more convenient
 
 shift = 0
-new_value_1 = value_shift(value_1)
-new_value_2 = value_shift(value_2)
+new_value_1 = int(value_shift(value_1))
+new_value_2 = int(value_shift(value_2))
 
 print(new_value_1)
 print(new_value_2)
@@ -35,10 +43,15 @@ print(shift)
 answer = 0
 for _ in range(new_value_1):
     answer += new_value_2
-answer = answer / (10 ** shift)
-print(answer)
+
+if shift != 0:
+    answer = str(answer)[:-shift] + "." + str(answer)[-shift:]
+print(f"{value_1} x {value_2} = {answer}")
+
 
 end_time = time.perf_counter()
 print(f"Time taken: {end_time - start_time}")
 
 # add negative number support :)
+# change calculation code so that it changes base and adds 0s to the end? could possibly work ngl...
+# maybe make a gui for it too using tkinter? LMAOOOOO
