530d427d4cd5e7c5a0e16d72ed6153c837ef7613
KaylaBytes <144823521+KaylaBytes@users.noreply.github.com>
2024-01-03 05:29:52 +0000

    Significant speed increase
    Instead of trying to over-engineer a fun (but impractical) solution, I decided to try use an actually practical solution. Suffice to say, the speed increase is insurmountable...

--- no_internal_mul.py
+++ no_internal_mul.py
@@ -15,10 +15,13 @@ start_time = time.perf_counter()
 
 def value_shift(value):
     global shift
+    global neg
     flip_value = list(str(value)[::-1])
-    print(flip_value)
     if flip_value[0] == "0":
         del flip_value[0:1]
+    if flip_value[-1] == "-":
+        del flip_value[-1]
+        neg += 1
     for x in enumerate(flip_value):
         if x[1] == ".":
             shift += x[0]
@@ -27,39 +30,21 @@ def value_shift(value):
 
 
 shift = 0
+neg = 0
 new_value_1 = int(value_shift(value_1))
 new_value_2 = int(value_shift(value_2))
 
-print(new_value_1)
-print(new_value_2)
-print(shift)
-
 # Calculation
 
-def calculation(base, multiplier):
-    magnitudes = [base, 1]
-    magadd = magnitudes[0]
-    while magadd <= multiplier:
-        for x in range(base - 1):
-            magadd += magnitudes[0]
-        magnitudes.insert(0, magadd)
-
-    based_number = []
-    for x in magnitudes:
-        total_mag = 0
-        while x <= multiplier:
-            multiplier -= x
-            total_mag += 1
-        based_number.append(total_mag)
-
-    del based_number[0]
-    based_number.append(0)
-
-    answer = 0
-    for x in enumerate(magnitudes):
-        for y in range(based_number[x[0]]):
-            answer += x[1]
-    return answer
+def calculation(smaller, larger):
+    split_small = list(str(smaller)[::-1]) # reverse smaller number to make calculation easier
+    totals = []
+    for x in enumerate(split_small):
+        placeholder = 0
+        for _ in range(int(x[1])):
+            placeholder += larger
+        totals.append(int(str(placeholder) + str(0) * x[0]))
+    return sum(totals)
 
 
 if new_value_1 - new_value_2 > 0:
@@ -70,19 +55,19 @@ else:
 
 if shift != 0:
     answer = str(answer)[:-shift] + "." + str(answer)[-shift:]
+if neg % 2 != 0:
+    answer = "-" + answer
 print(f"{value_1} x {value_2} = {answer}")
 
 
 end_time = time.perf_counter()
 print(f"Time taken: {end_time - start_time}")
 
-# add negative number support :)
 # maybe make a gui for it too using tkinter? LMAOOOOO
 # I really gotta figure out how classes work, as I've never learnt them and they seem necessary here
 
 
-# 4.5 and 3.6 took 0.0001405780203640461 seconds
-# 1003 and 2819 took 0.00018717796774581075 seconds
-# 23436362 and 54345342 took 0.8370736400247552 seconds
-# 10212.1213 and 2839472.65925 took 4.516522707999684 seconds
-# use 2839472.65925 and 10212.1213 as example in future, took too long this test
+# 4.5 and 3.6 took 0.0001009589759632945 seconds
+# 1003 and 2819 took 0.00011400901712477207 seconds
+# 23436362 and 54345342 took 0.00010792800458148122 seconds
+# 10212.1213 and 2839472.65925 took 0.00013760896399617195 seconds
